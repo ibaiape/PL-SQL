5c2d59c295612843d0ec74624a62d4bfb429f91f
ibaiape <ibai.apellaniz@ikasle.egibide.org>
2016-03-02 12:16:50 +0100

    add 4,5

--- Tareas a realizar 1.sql
+++ Tareas a realizar 1.sql
@@ -19,14 +19,32 @@ begin
 end;
 */
 --3
-
+/*
 declare
-V_MAX_ID NUMBER(2);
+V_MAX_ID CENTROS.ID%TYPE;
 begin
     select max(id) into V_MAX_ID from centros;
-    IF V_MAX_ID = null THEN
+    IF V_MAX_ID is null THEN
     DBMS_OUTPUT.put_line('No hay datos');
     ELSE
     DBMS_OUTPUT.put_line('El último identificador del centro es: '||V_MAX_ID);
     END IF;
 end;
+*/
+/*
+desc centros
+insert into centros values(0,'pepe','as',12,'01008','casa','prov','999');
+declare
+v_max_id centros.nombre%type;
+begin
+  Select nombre into v_max_id
+  from centros;
+    dbms_output.put_line('Encontrado nombre '||v_max_id);
+exception 
+  when NO_DATA_FOUND then
+  dbms_output.put_line('No encontrado');
+  when others then 
+  dbms_output.put_line('Algo pasó weee');
+end;
+*/
+/*4*/
---
+++ p1q4.sql
@@ -0,0 +1,22 @@
+declare
+  A CENTROS.ID%TYPE;
+  B CENTROS.NOMBRE%TYPE;
+begin
+  select CENTROS.NOMBRE INTO B FROM CENTROS WHERE UPPER(NOMBRE) = 'ABRERA';
+  
+  IF SQL%FOUND THEN
+    DBMS_OUTPUT.put_line('Ya hay un centro con el nombre Abrera');
+  END IF;
+exception
+  when NO_DATA_FOUND then
+    INSERT INTO CENTROS(NOMBRE)
+              VALUES('Abrera');
+    IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Ningun valor insertado');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Insertado el centro');
+        COMMIT;
+    END IF;
+    WHEN TOO_MANY_ROWS THEN
+        DBMS_OUTPUT.PUT_LINE('Hay más de un centro con el nombre Abrera');
+end;
